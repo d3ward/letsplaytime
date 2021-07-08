dae26be842970b14d827ecbe312caa88bfcdc878
Eduar Ursu <eduardursu78@gmail.com>
2021-07-09 00:14:41 +0200

    v0.0.1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# letsplay
+
+
+<p align="center"><img src="app-banner.png" alt="app-banner"/></p>
+<h1 align="center">Let's Play</h1>
+<p align="center">Progressive Web App built using HTML, CSS and Javascript.</p>
+<h3 align="center">Play <a href="https://d3ward.github.io/letsplay/">here</a></h3>
+<br>
+<p align="center"><img src="app-screenshot.png" alt="app-screenshot" width='600px'/></p>
---
+++ sw.js
@@ -0,0 +1,54 @@
+var cacheName = 'letsplay_v0.0.1';
+
+var appShellFiles = [
+    '.',
+    'index.html',
+    'script.js',
+    'style.css',
+    'manifest.json',
+    'confetti.browser.min.js',
+    'icons/favicon/favicon.ico'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(appShellFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((r) => {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener('message', function (event) {
+    if (event.data.action === 'skipWaiting') {
+        self.skipWaiting();
+    }
+});
