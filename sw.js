23d60709d3d8abbc078737cefc072315ca3d296e
Eduar Ursu <eduardursu78@gmail.com>
2021-07-19 00:18:00 +0200

    Update SW

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ var appShellFiles = [
 // Choose a cache name
 const cacheName = 'letsplay-v0.0.1';
 // List the files to precache
-const precacheResources = ['/', '/index.html', '/css/style.css', '/js/main.js', '/js/confetti.browser.min.js'];
+const precacheResources = ['./', './index.html', './css/style.css', './js/main.js', './js/confetti.browser.min.js'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
